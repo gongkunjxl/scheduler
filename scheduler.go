6f036e045b2d9420ec566ac01989cf8c7d485549
gongkunjxl <gongkunjxl@163.com>
2019-03-07 14:34:58 +0800

    random kubernetes mrws scheduler

--- scheduler.go
+++ scheduler.go
@@ -167,76 +167,104 @@ func (sch *Scheduler) KubernetesMaxScore(kubIdle *[PHYNUM][DIMENSION]float64, sa
 /*
  * MrwsSchedule : the mrws scheduler
  */
-func (sch *Scheduler) MrwsSchedule(podReq []PodRequest) []PodRequest {
+func (sch *Scheduler) MrwsSchedule(podReq []PodRequest, weightPod [][DIMENSION + 1]float64) []PodRequest {
 	//the mrwsUsed array
-	var mrwsUsed [PHYNUM][DIMENSION]float64
+	var mrwsUsed [PHYNUM][DIMENSION + 1]float64
 	for i := 0; i < PHYNUM; i++ {
-		for j := 0; j < DIMENSION; j++ {
+		for j := 0; j < DIMENSION+1; j++ {
 			mrwsUsed[i][j] = 1.0
 		}
 	}
 	// schedule all the pod
 	podLen := len(podReq)
 	for i := 0; i < podLen; i++ {
-		fitInd := sch.MrwsEvaluate(&mrwsUsed, podReq[i])
+		fitInd := sch.MrwsEvaluate(&mrwsUsed, podReq[i], &weightPod[i])
 		if fitInd != -1 {
 			// add the used resource
 			podReq[i].nodeName = fitInd
 			for j := 0; j < DIMENSION; j++ {
 				mrwsUsed[fitInd][j] = mrwsUsed[fitInd][j] + podReq[i].resReq[j]
 			}
-
+			mrwsUsed[fitInd][DIMENSION] = mrwsUsed[fitInd][DIMENSION] + 1.0
 		}
 	}
 	// calculate the cluster resource rate
-	sch.CalResourceRate(&mrwsUsed)
+	// sch.CalResourceRate(&mrwsUsed)
 
 	// calculate the balance value
-	sch.CalClusterBalance(&mrwsUsed, podReq)
+	// sch.CalClusterBalance(&mrwsUsed, podReq)
 
 	return podReq
 }
 
 /*
  * MrwsEvaluate : calculate the physical machine idle resource and determine the desitination
+ * mrwsUsed: the resource used , weightPod: the pod weight, podReq: the pod resource
  */
-func (sch *Scheduler) MrwsEvaluate(mrwsUsed *[PHYNUM][DIMENSION]float64, podReq PodRequest) int {
+func (sch *Scheduler) MrwsEvaluate(mrwsUsed *[PHYNUM][DIMENSION + 1]float64, podReq PodRequest, weightPod *[DIMENSION + 1]float64) int {
 	var fitInd int
 	fitInd = -1
-	// get the physical resource idle rate
+	// get the physical resource and pod idle rate
 	var mrwsIdle [PHYNUM][DIMENSION]float64
 	for i := 0; i < PHYNUM; i++ {
 		for j := 0; j < DIMENSION; j++ {
 			mrwsIdle[i][j] = (sch.reTotal[j] - mrwsUsed[i][j] - podReq.resReq[j]) / sch.reTotal[j]
 		}
 	}
+	var podSum float64
+	var podIdle [PHYNUM]float64
+	for i := 0; i < PHYNUM; i++ {
+		podSum = podSum + mrwsUsed[i][DIMENSION]
+	}
+	for i := 0; i < PHYNUM; i++ {
+		podIdle[i] = 1.0 - mrwsUsed[i][DIMENSION]/podSum
 
-	// get the satisfy physical machine index
+	}
+
+	// get the satisfy physical machine index and calculate the max value node
 	saInd := sch.ResourceSatisfy(&mrwsIdle)
 	if saInd != nil {
-		fitInd = sch.KubernetesMaxScore(&mrwsIdle, saInd)
-		// fmt.Printf("%d \n", fitInd)
+		saLen := len(saInd)
+		//calculate the satisfy index physical machine podMean and resMean
+
+		var resVal [DIMENSION]float64 // cal the sum and mean value
+		var resMean [DIMENSION]float64
+		var podVal float64
+		for i := 0; i < saLen; i++ {
+			podVal = podVal + podIdle[saInd[i]]
+			for j := 0; j < DIMENSION; j++ {
+				resVal[j] = resVal[j] + mrwsIdle[saInd[i]][j]
+			}
+		}
+		podMean := podVal / podSum
+		for j := 0; j < DIMENSION; j++ {
+			resMean[j] = resVal[j] / (float64)(saLen)
+		}
+
+		var maxScore float64
+		maxScore = -1.0
+		fitInd = saInd[0]
+		var bi, vi float64
+		for i := 0; i < saLen; i++ {
+			vi = 0.0
+			bi = 0.0
+			for j := 0; j < DIMENSION; j++ {
+				vi = vi + mrwsIdle[saInd[i]][j]*weightPod[j]
+				bi = bi + (mrwsIdle[saInd[i]][j]/resMean[j])*weightPod[DIMENSION]
+			}
+			vi = vi + podIdle[i]*weightPod[DIMENSION]
+			bi = bi + (podIdle[i]/podMean)*weightPod[DIMENSION]
+			fmt.Printf("vi and bi %.3f %.3f \n", vi, bi)
+			// bi = 0.0
+			scoreVi := vi + bi
+			if scoreVi > maxScore {
+				fitInd = saInd[i]
+				maxScore = scoreVi
+			}
+		}
+		// fmt.Printf("%.3f  %d \n", maxScore, fitInd)
 	}
 	return fitInd
-}
-
-/*
- * MrwsMaxScore : get the max score physical machine
- */
-func (sch *Scheduler) MrwsMaxScore(mrwsIdle *[PHYNUM][DIMENSION]float64, saInd []int) (maxInd int) {
-	saLen := len(saInd)
-	var maxScore float64
-	maxScore = -1.0
-	maxInd = saInd[0]
-	for i := 0; i < saLen; i++ {
-		scoreVal := mrwsIdle[saInd[i]][0]*0.5 + mrwsIdle[saInd[i]][1]*0.5
-		if scoreVal > maxScore {
-			maxScore = scoreVal
-			maxInd = saInd[i]
-		}
-		// fmt.Printf("%d %.3f %d \n", saInd[i], maxScore, maxInd)
-	}
-	return maxInd
 }
 
 /*
--- testFunction.go
+++ testFunction.go
@@ -4,19 +4,60 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
 /*
  * testScheduler : test random scheduler function
  */
-func testScheduler(sch *Scheduler) {
-	// build the test sample for random scheduler
-	var podRes = [DIMENSION]float64{300.0, 2000.0, 200.0, 100.0}
-	var podNum int
-	podNum = 13
-	var podReq []PodRequest
-	for i := 0; i < podNum; i++ {
+func testScheduler(sch *Scheduler, appPath string, weightPath string) {
+
+	fmt.Println("test scheduler functions")
+	podReq, weight := readApplication(appPath, weightPath)
+	podLen := len(podReq)
+	// for i := 0; i < appLen; i++ {
+	// 	fmt.Println(*podReq[i].resReq)
+	// 	fmt.Println(weight[i])
+	// }
+
+	// test the random scheduler function
+	// podList := sch.RandomSchedule(podReq)
+	// podList := sch.KubernetesSchedule(podReq)
+	podList := sch.MrwsSchedule(podReq, weight)
+
+	for i := 0; i < podLen; i++ {
+		fmt.Printf("%d ", podList[i].nodeName)
+	}
+	fmt.Println()
+}
+
+/*
+ * readApplication : read the application and application matrix
+ */
+func readApplication(appPath string, weightPath string) (podReq []PodRequest, weight [][DIMENSION + 1]float64) {
+	appFile, err := os.Open(appPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer appFile.Close()
+	appBuf := bufio.NewReader(appFile)
+	for {
+		line, err := appBuf.ReadString('\n')
+		line = strings.TrimSpace(line)
+		if err != nil || err == io.EOF {
+			break
+		}
+		var podRes [DIMENSION]float64
+		str := strings.Split(line, " ")
+		strLen := len(str)
+		for i := 0; i < strLen; i++ {
+			podRes[i], _ = strconv.ParseFloat(str[i], 64)
+		}
 		newPod := PodRequest{
 			resReq:   &podRes,
 			typePod:  "hadoop",
@@ -24,21 +65,38 @@ func testScheduler(sch *Scheduler) {
 		}
 		podReq = append(podReq, newPod)
 	}
-
-	fmt.Println("test scheduler functions")
-	// test the random scheduler function
-	//podList := sch.RandomSchedule(podReq)
-	podList := sch.KubernetesSchedule(podReq)
-
-	for i := 0; i < podNum; i++ {
-		fmt.Println(podList[i].nodeName)
+	// read weight matrix file
+	weightFile, err := os.Open(weightPath)
+	if err != nil {
+		panic(err.Error())
 	}
+	defer weightFile.Close()
+	weightBuf := bufio.NewReader(weightFile)
+	for {
+		line, err := weightBuf.ReadString('\n')
+		line = strings.TrimSpace(line)
+		if err != nil || err == io.EOF {
+			break
+		}
+		var weightM [DIMENSION + 1]float64
+		str := strings.Split(line, " ")
+		strLen := len(str)
+		for i := 0; i < strLen; i++ {
+			weightM[i], _ = strconv.ParseFloat(str[i], 64)
+		}
+		weight = append(weight, weightM)
+	}
+	return podReq, weight
 }
 
 /*
  * test main
  */
 func main() {
+	var appPath, weightPah string
+	appPath = "application.txt"
+	weightPah = "weight.txt"
+
 	var reTotal = [DIMENSION]float64{2400.0, 16000.0, 1000.0, 1000.0}
 	var thold float64
 	thold = 0.1
@@ -46,6 +104,6 @@ func main() {
 		reTotal: &reTotal,
 		thold:   thold,
 	}
-	testScheduler(sch)
+	testScheduler(sch, appPath, weightPah)
 
 }
